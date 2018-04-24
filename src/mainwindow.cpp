127499e71f6163b70234f31657ed1faa6a96cd01
Marcin Kisielewski <kisielewski.mar@gmail.com>
2018-04-24 10:59:06 +0200

    Updated mainwindow wih newboard, saveboard, loadboard, and exit actions. Also changed directory structure.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,9 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->actionSaveBoard->setDisabled(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
 }
