04423fc9e6a593efaf6bc09e74fbad166930921d
Unknown <kisielewski.mar@gmail.com>
2018-05-21 19:11:18 +0200

    Added documentation for rule dialog.

--- src/rulesdialog.h
+++ src/rulesdialog.h
@@ -13,19 +13,28 @@
 namespace Ui {
 class RulesDialog;
 }
-
+/*!
+ * \brief The RulesDialog class
+ */
 class RulesDialog : public QDialog
 {
 Q_OBJECT
 
 public:
+/*!
+ * \brief RulesDialog constructor.
+ * \param parent - pointer to parent object.
+ */
 explicit RulesDialog(QWidget *parent = 0);
 ~RulesDialog();
+/*!
+ * \brief Get rule from dialog list.
+ * \return selected rule from list.
+ */
 std::string GetRule();
 
 private slots:
 void on_btn_add_clicked();
-
 void on_btn_delete_clicked();
 
 private:
