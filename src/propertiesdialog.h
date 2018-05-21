770caffedb1efddef63a611834859a450d56fce3
Unknown <kisielewski.mar@gmail.com>
2018-05-21 19:03:08 +0200

    Added documentation for properties dialog

--- src/propertiesdialog.h
+++ src/propertiesdialog.h
@@ -6,14 +6,24 @@
 namespace Ui {
 class PropertiesDialog;
 }
-
+/*!
+ * \brief The PropertiesDialog class
+ */
 class PropertiesDialog : public QDialog
 {
 Q_OBJECT
 
 public:
+/*!
+ * \brief PropertiesDialog constructor.
+ * \param parent - pointer to parent object.
+ */
 explicit PropertiesDialog(QWidget *parent = 0);
 ~PropertiesDialog();
+/*!
+ * \brief Get simulation time from dialog.
+ * \return simulation time from properties dialog.
+ */
 int GetSimulationTime();
 
 private:
