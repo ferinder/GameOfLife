0c2294960bfc1940461e3085318600dd04e131d5
Unknown <kisielewski.mar@gmail.com>
2018-05-21 18:53:51 +0200

    Added documentation for GetRuleDialog.

--- src/getruledialog.h
+++ src/getruledialog.h
@@ -7,14 +7,26 @@
 namespace Ui {
 class GetRuleDialog;
 }
-
+/*!
+ * \brief The GetRuleDialog class
+ *
+ * Dialog class for retriving game rule from user.
+ */
 class GetRuleDialog : public QDialog
 {
 Q_OBJECT
 
 public:
+/*!
+ * \brief GetRuleDialog constructor.
+ * \param parent - pointer to arent object.
+ */
 explicit GetRuleDialog(QWidget *parent = 0);
 ~GetRuleDialog();
+/*!
+ * \brief Method returns user rule from dialog.
+ * \return user rule in string format.
+ */
 std::string GetRuleFromDialog();
 
 private:
