d842a229d5cfdd5e9636250af5e048f0cec94124
Marcin Kisielewski <kisielewski.mar@gmail.com>
2018-06-24 14:38:54 +0200

    Changed starting parameters in simulation time step spine in properties dialog

--- src/propertiesdialog.cpp
+++ src/propertiesdialog.cpp
@@ -6,6 +6,9 @@ PropertiesDialog::PropertiesDialog(QWidget *parent) :
     ui(new Ui::PropertiesDialog)
 {
     ui->setupUi(this);
+    ui->spin_sim_time->setMaximum(2000);
+    ui->spin_sim_time->setMinimum(100);
+    ui->spin_sim_time->setValue(500);
 }
 
 PropertiesDialog::~PropertiesDialog()
